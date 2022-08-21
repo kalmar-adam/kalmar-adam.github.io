6677a020ebf9c6aceed94607210dd0197931f656
kalmar-adam <63585863+kalmar-adam@users.noreply.github.com>
2022-08-21 20:14:13 +0200

    fixtry

--- myjs.js
+++ myjs.js
@@ -1,14 +1,10 @@
 //load projects
-var projects=[
-    ["Calendar Generator",
-    "With this you can generate a simple calendar for personal use.",
-    "/calendargen/"],
-    ["Flask-Forum",
-    "A simple forum made with Python Flask and it uses SQLite3.",
-    "https://github.com/kalmar-adam/Flask-Forum"]
+var projects = [
+  ["Calendar Generator", "With this you can generate a simple calendar for personal use.", "/calendargen/"],
+  ["Flask-Forum", "A simple forum made with Python Flask and it uses SQLite3.", "https://github.com/kalmar-adam/Flask-Forum"],
 ];
-var elements="";
+var elements = "";
 for (let i = 0; i < projects.length; i++) {
-    elements+="<a href=\""+projects[i][2]+"\" target='_blank'><h3>"+projects[i][0]+"</h3><p>"+projects[i][1]+"</p></a>";
+  elements += '<a href="' + projects[i][2] + "\" target='_blank'><h3>" + projects[i][0] + "</h3><p>" + projects[i][1] + "</p></a>";
 }
-document.getElementById('stuff_flex').innerHTML=elements;
+document.getElementById("stuff_flex").innerHTML = elements;
